d85a2f0c4d5e0df38805dedf51f9b0826c86d3cc
Hogale <262164@NTTDATA.COM>
2024-07-05 13:33:36 +0530

    script added

--- secondTestCase.py
+++ secondTestCase.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-from webdriver_manger.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 
